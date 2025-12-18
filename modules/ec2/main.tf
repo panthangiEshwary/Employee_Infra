66861578c9876bace6d9ab0d0467abc56661243c
panthangi Eshwary <77790524+panthangiEshwary@users.noreply.github.com>
2025-12-18 12:07:15 +0100

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,15 +1,14 @@
 resource "aws_instance" "employee_app_server" {
-  ami                         = var.employee_ami_id
-  instance_type               = "t3.micro"
-  key_name                    = var.employee_key_pair_name
-  subnet_id                   = var.public_subnet_id
-  vpc_security_group_ids      = [var.employee_ec2_sg_id]
-  associate_public_ip_address = true
-
-  user_data = file("${path.module}/userdata.sh")
+  ami                    = var.employee_ami_id
+  instance_type           = "t3.micro"
+  key_name               = var.employee_key_pair_name
+  subnet_id              = var.public_subnet_id
+  vpc_security_group_ids = [var.employee_ec2_sg_id]
 
   associate_public_ip_address = true
   iam_instance_profile        = aws_iam_instance_profile.employee_ec2_profile.name
+
+  user_data = file("${path.module}/userdata.sh")
 
   lifecycle {
     create_before_destroy = true
@@ -20,4 +19,3 @@ resource "aws_instance" "employee_app_server" {
     Project = "Employee-App"
   }
 }
-
