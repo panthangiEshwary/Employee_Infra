1f210734dc69256e1b81fa797fe65911f1492518
panthangi Eshwary <77790524+panthangiEshwary@users.noreply.github.com>
2025-12-20 09:38:29 +0100

    Update main.tf

--- modules/ec2_mon/main.tf
+++ modules/ec2_mon/main.tf
@@ -6,14 +6,14 @@ resource "aws_instance" "monitor_instance" {
   vpc_security_group_ids      = [var.monitor_sg_id]
   associate_public_ip_address = true
 
-   user_data = templatefile("${path.module}/userdata.sh", {
+  user_data = templatefile("${path.module}/userdata.sh", {
     prometheus_config = templatefile("${path.module}/prometheus.yml.tpl", {
       app_private_ip = var.app_private_ip
     })
 
-    alert_rules_config  = file("${path.module}/alert-rules.yml.tpl")
-    alertmanager_config = file("${path.module}/alertmanager.yml.tpl")
-    docker_compose      = file("${path.module}/docker-compose.yml.tpl")
+    alert_rules_config  = templatefile("${path.module}/alert-rules.yml.tpl", {})
+    alertmanager_config = templatefile("${path.module}/alertmanager.yml.tpl", {})
+    docker_compose      = templatefile("${path.module}/docker-compose.yml.tpl", {})
   })
 
   tags = {
